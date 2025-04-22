d24ce5c223f58c2bf47da113f4c80a9b8ea40202
shadowfeind <travestroy007@gmail.com>
2025-04-22 20:43:17 +0545

    using payload

--- db/client.ts
+++
@@ -1,8 +0,0 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
-import * as schema from "./schema/index"; // Import all schemas
-
-const connectionString = process.env.DATABASE_URL!;
-const client = postgres(connectionString);
-
-export const db = drizzle(client, { schema, logger: true });
